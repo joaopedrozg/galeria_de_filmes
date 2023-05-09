dcf88bd4dd4f812a1272bf4b1e2fa9fbd76b709c
Joao Pedro Zanetti Goncalves <joao.pedrozg@hotmail.com>
2023-05-09 14:11:27 -0300

    criando classe User/ UserDAO/ e interface para a manipulação de usuários

---
+++ auth_process.php
@@ -0,0 +1,43 @@
+<?php
+
+use function PHPSTORM_META\type;
+
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/User.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+
+
+    $message = new Message($BASE_URL);
+
+
+
+    // Resgata o tipo do formulário
+
+
+    $type = filter_input(INPUT_POST, "type");
+
+    // verifica o tipo do formulário
+
+    if($type === "register"){
+
+        $name = filter_input(INPUT_POST, "name");
+        $lastname = filter_input(INPUT_POST, "lastname");
+        $email = filter_input(INPUT_POST, "email");
+        $password = filter_input(INPUT_POST, "password");
+        $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+        
+        // Verificação de dados mínimos
+        if($name && $lastname && $email && $password){
+
+
+        } else {
+            // Enviar msg de erro, de dados faltantes
+            $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
+
+        }
+
+    } else if($type === "login"){
+
+    }
